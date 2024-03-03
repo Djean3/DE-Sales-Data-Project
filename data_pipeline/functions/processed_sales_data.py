7f3028b55346041bba6030f36f1f1d06e8d009a3
David Jean <davidejeaniii@gmail.com>
2024-03-03 14:51:08 -0600

    Add initial functions

---
+++ data_pipeline/functions/processed_sales_data.py
@@ -0,0 +1,16 @@
+import os
+
+import awswrangler as wr
+from aws_lambda_powertools import Logger
+from helpers.datacleaning import drop_na, rename_avg_price_to_price
+
+
+logger = Logger(level=os.environ["LOG_THRESHOLD"])
+
+
+def process_sales_data(event, context):
+    df = wr.s3.read_csv("s3://de-sales-data-project-raw-data-146479615822/Online_Shopping_Dataset.csv")
+    df = drop_na(df)
+    df = rename_avg_price_to_price(df)
+    df = df.drop(columns=['Unnamed: 0', 'Date'])
+    return {"status": "success", "message": "hello world"}
---
+++ data_pipeline/helpers/datacleaning.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import awswrangler as wr
+
+def drop_na(df):
+    """Drop na values from dataframe
+
+    Args:
+        df (dataframe): sales data dataframe
+
+    Returns:
+        dataframe: sales data dataframe with na values dropped
+    """
+    clean_data = df.dropna()
+    return clean_data
+
+def rename_avg_price_to_price(df):
+    """rename 'Avg_Price' column to 'Price'
+
+    Args:
+        df (dataframe): sales data dataframe
+
+    Returns:
+        dataframe: df with updated column name
+    """    
+    df_renamed = df.rename(columns={'Avg_Price': 'Price'})
+    return df_renamed
+
+
