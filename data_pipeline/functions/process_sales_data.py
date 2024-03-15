00e986e97b91c1b3f7d0151d774aa2e24f6bce74
David Jean <davidejeaniii@gmail.com>
2024-03-15 14:16:04 -0500

    added more functions with doc strings

--- data_pipeline/functions/process_sales_data.py
+++ data_pipeline/functions/process_sales_data.py
@@ -11,5 +11,8 @@ def process_sales_data(event, context):
     df = drop_na(df)
     df = rename_avg_price_to_price(df)
     df = df.drop(columns=["Unnamed: 0", "Date"])
+    df = remove_trailing_zeros_from_df(df)
+    df = update_online_spend_with_quantity(df)
+    df = total_spend(df)
     df.to_csv("processed_file.csv", index=False)
     return {"status": "success", "message": "hello world"}
