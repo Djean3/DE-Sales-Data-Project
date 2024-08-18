e04a485686b2d7492190a1aa39378dc67828eb7f
David Jean <davidejeaniii@gmail.com>
2024-08-18 18:46:55 -0500

    updated

--- data_visualization/streamlit_app.py
+++ data_visualization/streamlit_app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 from src.data_loader import read_data_from_s3
 import plotly.express as px
+import awswrangler as wr
 
 st.title("Streams of Data Streamlit Example")
 
-st.header("Data from S3")
+st.header("Data from S3-2")
+df = wr.s3.read_parquet("s3://de-sales-data-project-data-lake-146479615822/sales_data/")
 
-s3_df = read_data_from_s3(f"s3://de-sales-data-project-data-lake-146479615822/sales_data/")
 
-st.dataframe(s3_df)
+st.dataframe(df)
